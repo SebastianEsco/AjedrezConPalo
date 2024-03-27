4a8d037c7272f03058b66df347615a6fbd951ef5
SebastianEsco <sebastian.escobarm@upb.edu.co>
2024-03-27 10:03:26 -0500

    Algunos cambios
    hola

---
+++ Ajedrez/Assets/Scripts/Peon.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Peon : Pieza
+{
+    public Peon(bool color, int valor, int fila, int columna ) : base(color, valor, fila, columna)
+    {
+
+    }
+
+    public void IMover(int fila, int columna)
+    {
+
+    }
+
+}
